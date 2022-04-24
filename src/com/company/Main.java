97a260596117354a448539d4c2aeed1a79d545d6
SosoBurger <stulnikovan1337@gmail.com>
2022-04-24 12:29:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input;
+        Scanner in = new Scanner(System.in);
+        while(true){
+            input = in.nextLine();
+            switch (input){
+                case ("H"):
+                case ("h"):{
+                    System.out.println("Hello World!");
+                    break;
+                }
+                case("Q"):
+                case("q"):{
+                    System.out.println("True or False?");
+                    input = in.nextLine();
+                    if(input.equalsIgnoreCase("true")|input.equals("1")){
+                        System.out.println("True!");
+                    }
+                    else{
+                        System.out.println("False!");
+                    }
+                    break;
+                }
+                case("+"):{
+                    System.out.println("Введите число");
+                    try {
+                        int j = in.nextInt();
+                        for (int i = 1; i <= j; i++){
+                            Thread.sleep(500);
+                            System.out.println(i);
+                        }
+                    } catch (Exception ex) {
+                        System.out.println("Норм с головой?");
+                    }
+                }
+            }
+        }
+    }
+}
